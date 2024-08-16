51b263fbd2c4302484853fb7ccb0fd79567ec56e
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2024-08-16 11:58:42 -0300

    dsfsf

---
+++ Fiap2Sem/Python/Aula3/tabelas.py
@@ -0,0 +1,34 @@
+def lista_tabela(t: list) -> None:
+    for i in range(0, len(t), 1):
+        print(f"Registro: {i+1}")
+        print(f"Nome........: {t[i]['nome']}")
+        print(f"Idade........: {t[i]['idade']}")
+ 
+import os
+os.system("cls")
+
+tabela = []
+dicionario = {
+    'nome': 'Edson',
+    'idade': 50
+}
+tabela.append(agenda.copy())
+ 
+agenda['nome'] = input("Nome: ")
+agenda['idade'] = input("idade: ")
+ 
+tabela.append(agenda.copy())
+ 
+agenda['nome'] = input("Nome: ")
+agenda['idade'] = input("idade: ")
+ 
+tabela.append(agenda.copy())
+ 
+for i in range(0, len(tabela), 1):
+    print(f"Registro: {i+1}")
+    print(f"Nome........: {tabela[i]['nome']}")
+    print(f"Idade........: {tabela[i]['idade']}")
+ 
+ 
+ 
+lista_tabela(tabela)

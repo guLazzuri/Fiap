574ea1cc27751e976f4d3c70e5310eeaaaa5a5f5
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2024-10-29 08:41:46 -0300

    pc (commit VS)

--- Fiap2Sem/Front-End/next/src/app/compras/page.tsx
+++ Fiap2Sem/Front-End/next/src/app/compras/page.tsx
@@ -2,7 +2,7 @@ export default function Compras(){
 
     return(
         <main className="flex justify-center p-10">
-      <h1 className="text-center text-4x1 font-bold text-indigo-800">Compras</h1>
+      <h1 className="text-center text-4x1 font-bold text-indigo-800 text-3xl">Compras</h1>
     </main>
     )
 }
--- Fiap2Sem/Front-End/next/src/app/page.tsx
+++ Fiap2Sem/Front-End/next/src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex justify-center p-10">
-      <h1 className="text-center text-4x1 font-bold text-indigo-800">Home</h1>
+      <h1 className="text-center text-4x1 font-bold text-indigo-800 text-3xl">Home</h1>
     </main>
   );
 }

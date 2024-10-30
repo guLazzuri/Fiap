e28eed4f90519c2a28dd7c8f5bacfc4dba0056e1
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2024-10-30 09:38:00 -0300

    pc2

--- Fiap2Sem/Front-End/next/src/app/compras/page.tsx
+++ Fiap2Sem/Front-End/next/src/app/compras/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+ 
+import { AuthContext } from "@/context/page"
+import { redirect } from "next/navigation"
+import { useContext } from "react"
+ 
 export default function Compras(){
-
+ 
+    const {user} = useContext(AuthContext)
+ 
+    if(user?.nome ==""){
+        alert ("Para fazer compras, antes faça o login")
+        redirect("/")
+    }
+ 
     return(
-        <main className="flex justify-center p-10">
-      <h1 className="text-center text-4x1 font-bold text-indigo-800 text-3xl">Compras</h1>
-    </main>
+        <main className="flex flex-col items-center justify-center p-10">
+            <h1 className="text-center text-4xl font-bold text-indigo-800">Página de Compras</h1>
+ 
+            <p className="text-3xl font-black">Ola {user?.nome}, faça suas compras!</p>
+        </main>
     )
 }
--- Fiap2Sem/Front-End/next/src/app/layout.tsx
+++ Fiap2Sem/Front-End/next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Menu from "@/Components/Menu";
+import { AuthProvider } from "@/context/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Menu/>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
